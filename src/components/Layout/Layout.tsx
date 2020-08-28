88b7314dcdb40e53e9a51d6150d2c1fd53996d96
Kee Tee <tkeeching@gmail.com>
2020-08-28 13:45:29 +1000

    Add title

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,31 @@
 import React from "react"
 import "./Layout.sass"
+import { useStaticQuery, graphql } from "gatsby"
+import { Helmet } from "react-helmet"
 
-const Layout: React.FC = ({ children }) => (
-  <div className="layout-grid-container">
-    { children }
-  </div>
-)
+const Layout: React.FC = ({ children }) => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+          }
+        }
+      }
+    `
+  )
+
+  return (
+    <div className="layout-grid-container">
+      <Helmet>
+        <title>{data.site.siteMetadata.title}</title>
+        <meta name="description" content={data.site.siteMetadata.description} />
+      </Helmet>
+      {children}
+    </div>
+  )
+}
 
 export default Layout
